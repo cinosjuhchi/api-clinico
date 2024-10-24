951176ac3211822ea10ae07b18b9cc84481bd259
cinosjuhchi <pacino447@gmail.com>
2024-10-24 20:43:03 +0700

    s

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,8 @@ use App\Http\Controllers\Api\V1\UserController;
 Route::get('/', function (){
     // Di controller atau tempat Anda mengakses file
     dd([
-        'file_exists' => Storage::disk('public')->exists('image_profile/rrjiSHxPY7VBbQMzW1bFxVOrUiletjbIRmh3eBWJ.png'),
-        'full_path' => storage_path('app/public/image_profile/rrjiSHxPY7VBbQMzW1bFxVOrUiletjbIRmh3eBWJ.png'),
-        'storage_url' => Storage::url('image_profile/rrjiSHxPY7VBbQMzW1bFxVOrUiletjbIRmh3eBWJ.png')
+        'file_exists' => Storage::disk('public')->exists('image_profile/Q0UQIET5vipCAsSUNNEc6den3782kZa2hHJ3cZQf.png'),
+        'full_path' => storage_path('app/public/image_profile/Q0UQIET5vipCAsSUNNEc6den3782kZa2hHJ3cZQf.png'),
+        'storage_url' => Storage::url('image_profile/Q0UQIET5vipCAsSUNNEc6den3782kZa2hHJ3cZQf.png')
     ]);    
 });

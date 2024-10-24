fdb8c241f0fd0cc19b74ef22e92087cd65f22c42
cinosjuhchi <pacino447@gmail.com>
2024-10-24 19:30:13 +0700

    test

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,16 @@
 <?php
 
-use App\Http\Controllers\Api\V1\UserController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
+use App\Http\Controllers\Api\V1\UserController;
 
 
 Route::get('/', function (){
+    // Di controller atau tempat Anda mengakses file
+    dd([
+        'file_exists' => Storage::disk('public')->exists('image_profile/rrjiSHxPY7VBbQMzW1bFxVOrUiletjbIRmh3eBWJ.png'),
+        'full_path' => storage_path('app/public/image_profile/rrjiSHxPY7VBbQMzW1bFxVOrUiletjbIRmh3eBWJ.png'),
+        'storage_url' => Storage::url('image_profile/rrjiSHxPY7VBbQMzW1bFxVOrUiletjbIRmh3eBWJ.png')
+    ]);
     return view('welcome');
 });

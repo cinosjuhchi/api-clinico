1af6c493b30e48ce19bf1eea1602f752d0b5e216
cinosjuhchi <pacino447@gmail.com>
2024-10-24 20:45:18 +0700

    file debugging

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Api\V1\UserController;
 Route::get('/', function (){
     // Di controller atau tempat Anda mengakses file
     dd([
+        'file' => Storage::disk('public')->get('image_profile/Q0UQIET5vipCAsSUNNEc6den3782kZa2hHJ3cZQf.png'),
         'file_exists' => Storage::disk('public')->exists('image_profile/Q0UQIET5vipCAsSUNNEc6den3782kZa2hHJ3cZQf.png'),
         'full_path' => storage_path('app/public/image_profile/Q0UQIET5vipCAsSUNNEc6den3782kZa2hHJ3cZQf.png'),
         'storage_url' => Storage::url('image_profile/Q0UQIET5vipCAsSUNNEc6den3782kZa2hHJ3cZQf.png')

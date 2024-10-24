265cc97ed1d786142f154228049ce9d17d44fee3
cinosjuhchi <pacino447@gmail.com>
2024-10-24 19:09:55 +0700

    fix

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,7 +40,8 @@ return [
             'driver' => 'local',
             'root' => storage_path('app/public'),
             'url' => env('APP_URL').'/storage',
-            'visibility' => 'public',            
+            'visibility' => 'public',
+            'throw' => false,
         ],
 
         's3' => [

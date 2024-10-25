e79edc06ca62a761965e4028ac09d8b44bcb08de
cinosjuhchi <pacino447@gmail.com>
2024-10-25 15:25:41 +0700

    fix bug

--- routes/api.php
+++ routes/api.php
@@ -186,8 +186,7 @@ Route::prefix('v1')->group(function () {
             Route::prefix('doctor')->group(function () {
                 Route::get('/', [ClinicDataController::class, 'doctors']);
                 Route::get('/show/{doctor}', [ClinicDataController::class, 'showDoctor']);
-                Route::post('store', [ClinicDataController::class, 'storeDoctor']);                
-                Route::put('/update/{doctor}', [ClinicDataController::class, 'updateDoctor']);                
+                Route::post('store', [ClinicDataController::class, 'storeDoctor']);                            
             });
         });
     });

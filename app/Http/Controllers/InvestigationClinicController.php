57cce2e1b66d62c626941959c2175c47d22ed465
cinosjuhchi <pacino447@gmail.com>
2024-10-27 03:42:14 +0700

    read investigation

--- app/Http/Controllers/InvestigationClinicController.php
+++ app/Http/Controllers/InvestigationClinicController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\DB;
 use App\Models\InvestigationClinic;
@@ -14,9 +15,32 @@ class InvestigationClinicController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $user = Auth::user();
+        $clinic = $user->clinic;
+
+        if(!$clinic)
+        {
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'Clinic not found'
+            ], 403);
+        }
+        $query = $request->input('q');
+        $investigations = $clinic->investigations()
+        ->withCount('items') // Count the related items
+        ->when($query, function ($q) use ($query) {
+            $q->where('name', 'like', "%{$query}%") // Search by investigation name
+              ->orHaving('items_count', '=', $query); // Search by items count
+        })
+        ->paginate(10);
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Successfully fetched data',
+            'data' => $investigations
+        ]);
     }
 
     /**

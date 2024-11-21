9503d6b6b9d0e50aff81582f01c5051c695896ed
cinosjuhchi <pacino447@gmail.com>
2024-11-21 19:45:05 +0700

    staff login logout

---
+++ app/Http/Controllers/StaffAuthController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\DoctorResource;
+use App\Models\Staff;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class StaffAuthController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    public function me(Request $request)
+    {
+        $user = Auth::user();
+        if ($user->role != 'staff') {
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'Invalid Role Access',
+            ], 401);
+        }
+        $staff = $user->staff;
+        $staff->with([
+            'clinic',
+        ]);
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Fetch doctor profile is successfully!',
+            'data' => new DoctorResource($doctor),
+        ]);
+
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return response()->json(["message" => "Logout"], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Staff $staff)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Staff $staff)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Staff $staff)
+    {
+        //
+    }
+}

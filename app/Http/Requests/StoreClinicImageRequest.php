a50334308bdd51a7b7bb9a0513fe4a397bdc8399
cinosjuhchi <pacino447@gmail.com>
2024-11-30 23:19:35 +0700

    commit validation

--- app/Http/Requests/StoreClinicImageRequest.php
+++ app/Http/Requests/StoreClinicImageRequest.php
@@ -22,6 +22,7 @@ class StoreClinicImageRequest extends FormRequest
     public function rules(): array
     {
         return [
+            'images' => 'array|required',
             'images.*' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',            
         ];
     }

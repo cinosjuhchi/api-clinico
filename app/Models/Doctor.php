d9532451ae45a5942d7fe0cc0beddc8d73e1fcff
cinosjuhchi <pacino447@gmail.com>
2024-11-30 22:40:47 +0700

    add relationship

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -33,6 +33,11 @@ class Doctor extends Authenticatable
         return $this->belongsTo(Category::class, 'category_id');
     }
 
+    public function user()
+    {
+        return $this->belongsTo(User::class,'user_id');
+    }
+
     public function schedules(): HasMany
     {
         return $this->hasMany(DoctorSchedule::class, 'doctor_id');

8af77bd5c304944b581ab4605c22a0afe50e5910
cinosjuhchi <pacino447@gmail.com>
2024-12-06 11:41:17 +0700

    fix clinic location

--- app/Models/ClinicLocation.php
+++ app/Models/ClinicLocation.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class ClinicLocation extends Model
 {
     use HasFactory;
-
+    protected $guarded = ['id'];
     public function clinic(): BelongsTo 
     {
         return $this->belongsTo(Clinic::class);

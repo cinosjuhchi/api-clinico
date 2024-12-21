bcff54912c62918d93d16060c52e806298a58ad2
cinosjuhchi <pacino447@gmail.com>
2024-12-21 20:43:02 +0700

    add riskFactor

---
+++ app/Http/Controllers/RiskFactorController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\RiskFactor;
+use App\Http\Requests\StoreRiskFactorRequest;
+use App\Http\Requests\UpdateRiskFactorRequest;
+
+class RiskFactorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreRiskFactorRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(RiskFactor $riskFactor)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(RiskFactor $riskFactor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateRiskFactorRequest $request, RiskFactor $riskFactor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(RiskFactor $riskFactor)
+    {
+        //
+    }
+}

1cb4acc1879db0e0faed2702c3bcb31eaeffb215
cinosjuhchi <pacino447@gmail.com>
2025-01-26 09:13:58 +0700

    .

--- database/migrations/2025_01_26_021223_add_total_sales_to_clinic_settlement.php
+++ database/migrations/2025_01_26_021223_add_total_sales_to_clinic_settlement.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('clinic_settlements', function (Blueprint $table) {
-            $table->decimal('total_sales_cash', 12, 2);
+            $table->decimal('total_sales_cash', 12, 2)->nullable();
 
         });
     }

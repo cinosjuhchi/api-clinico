f6936cc6e9aaa475613b615e6fbd8accfaa1434f
cinosjuhchi <pacino447@gmail.com>
2025-02-16 08:43:10 +0700

    fix getMy revenue

--- routes/api.php
+++ routes/api.php
@@ -302,7 +302,7 @@ Route::prefix('v1')->group(function () {
                 Route::put('/call-patient/{appointment}', [ConsultationController::class, 'callPatient']);
             });
             Route::prefix('revenue')->group(function () {
-                Route::get('/', [BillController::class, 'myRevenue']);
+                Route::get('/', [BillController::class, 'getMyRevenue']);
             });
         });
     });

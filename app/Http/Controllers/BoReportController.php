7774020433c7705795835f88b3b4d71920ace493
cinosjuhchi <pacino447@gmail.com>
2025-02-28 17:59:30 +0700

    push

--- app/Http/Controllers/BoReportController.php
+++ app/Http/Controllers/BoReportController.php
@@ -10,21 +10,35 @@ class BoReportController extends Controller
     public function totalSales(Request $request)
     {
         $validated = $request->validate([
-            'from_date' => 'required|date',
-            'to_date' => 'required|date'
+            'from_date' => 'required|date|before:to_date',
+            'to_date' => 'required|date|after:from_date'
         ]);
 
-        $totalSales = Billing::whereBetween('transaction_date', [$validated['from_date'], $validated['to_date']])
+        $transactions = Billing::whereBetween('transaction_date', [$validated['from_date'], $validated['to_date']])
             ->where('is_paid', true)
             ->with(['clinic', 'patient.demographics'])
-            ->selectRaw('transaction_date, SUM(total_cost) as total_sales, COUNT(DISTINCT patient_id) as total_patient')
+            ->get()
             ->groupBy('transaction_date')
-            ->get();
+            ->map(function ($items, $date) {
+                return [
+                    'transaction_date' => $date,
+                    'total_sales' => $items->sum('total_cost'),
+                    'total_patient' => $items->unique('patient_id')->count(),
+                    'transaction' => $items->map(function ($item) {
+                        return [
+                            'clinic' => $item->clinic,
+                            'patient' => $item->patient
+                        ];
+                    })->values()
+                ];
+            })
+            ->values(); // Mengembalikan array numerik
 
         return response()->json([
             'status' => 'success',
-            'data'   => $totalSales
+            'data'   => $transactions
         ], 200);
     }
+
 
 }

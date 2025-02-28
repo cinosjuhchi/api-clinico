abdfea9df71e6931fefaa5147f7b7ec1d90e2272
cinosjuhchi <pacino447@gmail.com>
2025-02-28 17:42:40 +0700

    fix again

--- app/Http/Controllers/BoReportController.php
+++ app/Http/Controllers/BoReportController.php
@@ -14,11 +14,16 @@ class BoReportController extends Controller
             'to_date' => 'required|date|after:from_date'
         ]);
 
-        
-        
+        $totalSales = Billing::whereBetween('transaction_date', [$validated['from_date'], $validated['to_date']])
+            ->where('is_paid', true)
+            ->with(['clinic', 'patient.demographics'])
+            ->selectRaw('transaction_date, SUM(total_cost) as total_sales, COUNT(DISTINCT patient_id) as total_patient')
+            ->groupBy('transaction_date')
+            ->get();
+
         return response()->json([
             'status' => 'success',
-            'data'   => 'hello'
+            'data'   => $totalSales
         ], 200);
     }
 
